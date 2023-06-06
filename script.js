271cd04d4a32ad241e502c0a41c503f5e01880ad
phounphonh.ludovic@gmail.com <phounphonh.ludovic@gmail.com>
2023-06-06 16:04:23 +0200

    add card popup detail kombucha

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ function toggleNav(){
     navigation.classList.toggle('active')
 }
 
+
+
 const btnKombucha1 = document.querySelector('.kombucha1');
 const infoKombucha1 = document.querySelector('.detail-kombucha1');
 btnKombucha1.addEventListener('click', () =>{
@@ -21,6 +23,7 @@ btnKombucha1.addEventListener('click', () =>{
         infoKombucha1.style.display = 'flex';
     }
 });
+
 const btnKombucha2 = document.querySelector('.kombucha2');
 const infoKombucha2 = document.querySelector('.detail-kombucha2');
 btnKombucha2.addEventListener('click', () =>{
@@ -30,6 +33,7 @@ btnKombucha2.addEventListener('click', () =>{
         infoKombucha2.style.display = 'flex';
     }
 });
+
 const btnKombucha3 = document.querySelector('.kombucha3');
 const infoKombucha3 = document.querySelector('.detail-kombucha3');
 btnKombucha3.addEventListener('click', () =>{
@@ -39,6 +43,7 @@ btnKombucha3.addEventListener('click', () =>{
         infoKombucha3.style.display = 'flex';
     }
 });
+
 const btnKombucha4 = document.querySelector('.kombucha4');
 const infoKombucha4 = document.querySelector('.detail-kombucha4');
 btnKombucha4.addEventListener('click', () =>{
