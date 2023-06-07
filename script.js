e38eab67f13c2241f0279c7f0d8136a96f49b202
phounphonh.ludovic@gmail.com <phounphonh.ludovic@gmail.com>
2023-06-07 12:16:18 +0200

    change option du display none card detail product

--- script.js
+++ script.js
@@ -16,40 +16,52 @@ function toggleNav(){
 
 const btnKombucha1 = document.querySelector('.kombucha1');
 const infoKombucha1 = document.querySelector('.detail-kombucha1');
-btnKombucha1.addEventListener('click', () =>{
-    if(infoKombucha1.style.display == 'flex'){
+btnKombucha1.addEventListener('click', function() {
+    infoKombucha1.style.display = 'flex';
+});
+document.addEventListener('click', function(event) {
+    let elementClick = event.target;
+    if (elementClick != btnKombucha1 && !infoKombucha1.contains(elementClick)){
         infoKombucha1.style.display = 'none';
-    } else {
-        infoKombucha1.style.display = 'flex';
     }
 });
-
+// -------------------------------
 const btnKombucha2 = document.querySelector('.kombucha2');
 const infoKombucha2 = document.querySelector('.detail-kombucha2');
-btnKombucha2.addEventListener('click', () =>{
-    if(infoKombucha2.style.display == 'flex'){
+btnKombucha2.addEventListener('click', function() {
+    infoKombucha2.style.display = 'flex';
+});
+document.addEventListener('click', function(event) {
+    let elementClick = event.target;
+    if (elementClick != btnKombucha2 && !infoKombucha2.contains(elementClick)){
         infoKombucha2.style.display = 'none';
-    } else {
-        infoKombucha2.style.display = 'flex';
     }
 });
+// --------------------------------
 
 const btnKombucha3 = document.querySelector('.kombucha3');
 const infoKombucha3 = document.querySelector('.detail-kombucha3');
-btnKombucha3.addEventListener('click', () =>{
-    if(infoKombucha3.style.display == 'flex'){
+btnKombucha3.addEventListener('click', function() {
+    infoKombucha3.style.display = 'flex';
+});
+document.addEventListener('click', function(event) {
+    let elementClick = event.target;
+    if (elementClick != btnKombucha3 && !infoKombucha3.contains(elementClick)){
         infoKombucha3.style.display = 'none';
-    } else {
-        infoKombucha3.style.display = 'flex';
     }
 });
 
+//----------------------------------
 const btnKombucha4 = document.querySelector('.kombucha4');
 const infoKombucha4 = document.querySelector('.detail-kombucha4');
-btnKombucha4.addEventListener('click', () =>{
-    if(infoKombucha4.style.display == 'flex'){
+btnKombucha4.addEventListener('click', function() {
+    infoKombucha4.style.display = 'flex';
+});
+document.addEventListener('click', function(event) {
+    let elementClick = event.target;
+    if (elementClick != btnKombucha4 && !infoKombucha4.contains(elementClick)){
         infoKombucha4.style.display = 'none';
-    } else {
-        infoKombucha4.style.display = 'flex';
     }
 });
+
+
