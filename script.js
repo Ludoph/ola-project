2890e7de0e17084a662c13ddab5b974af705fd4d
phounphonh.ludovic@gmail.com <phounphonh.ludovic@gmail.com>
2023-06-12 12:44:37 +0200

    fix card services

--- script.js
+++ script.js
@@ -14,85 +14,101 @@ function toggleNav(){
 //----------------------------------
 const btnKombucha1 = document.querySelector('.kombucha1');
 const infoKombucha1 = document.querySelector('.detail-kombucha1');
-btnKombucha1.addEventListener('click', function() {
-    infoKombucha1.style.display = 'flex';
-});
-document.addEventListener('click', function(event) {
-    let elementClick = event.target;
-    if (elementClick != btnKombucha1 && !infoKombucha1.contains(elementClick)){
-        infoKombucha1.style.display = 'none';
-    }
-});
+if (btnKombucha1 != undefined){
+    btnKombucha1.addEventListener('click', function() {
+        infoKombucha1.style.display = 'flex';
+    });
+    document.addEventListener('click', function(event) {
+        let elementClick = event.target;
+        if (elementClick != btnKombucha1 && !infoKombucha1.contains(elementClick)){
+            infoKombucha1.style.display = 'none';
+        }
+    });
+}
 // -------------------------------
 const btnKombucha2 = document.querySelector('.kombucha2');
 const infoKombucha2 = document.querySelector('.detail-kombucha2');
-btnKombucha2.addEventListener('click', function() {
-    infoKombucha2.style.display = 'flex';
-});
-document.addEventListener('click', function(event) {
-    let elementClick = event.target;
-    if (elementClick != btnKombucha2 && !infoKombucha2.contains(elementClick)){
-        infoKombucha2.style.display = 'none';
-    }
-});
+if (btnKombucha2 != undefined){
+
+    btnKombucha2.addEventListener('click', function() {
+        infoKombucha2.style.display = 'flex';
+    });
+    document.addEventListener('click', function(event) {
+        let elementClick = event.target;
+        if (elementClick != btnKombucha2 && !infoKombucha2.contains(elementClick)){
+            infoKombucha2.style.display = 'none';
+        }
+    });
+}
 // --------------------------------
 const btnKombucha3 = document.querySelector('.kombucha3');
 const infoKombucha3 = document.querySelector('.detail-kombucha3');
-btnKombucha3.addEventListener('click', function() {
-    infoKombucha3.style.display = 'flex';
-});
-document.addEventListener('click', function(event) {
-    let elementClick = event.target;
-    if (elementClick != btnKombucha3 && !infoKombucha3.contains(elementClick)){
-        infoKombucha3.style.display = 'none';
-    }
-});
+if (btnKombucha3 != undefined){
+    btnKombucha3.addEventListener('click', function() {
+        infoKombucha3.style.display = 'flex';
+    });
+    document.addEventListener('click', function(event) {
+        let elementClick = event.target;
+        if (elementClick != btnKombucha3 && !infoKombucha3.contains(elementClick)){
+            infoKombucha3.style.display = 'none';
+        }
+    });
+}
 //----------------------------------
 const btnKombucha4 = document.querySelector('.kombucha4');
 const infoKombucha4 = document.querySelector('.detail-kombucha4');
-btnKombucha4.addEventListener('click', function() {
-    infoKombucha4.style.display = 'flex';
-});
-document.addEventListener('click', function(event) {
-    let elementClick = event.target;
-    if (elementClick != btnKombucha4 && !infoKombucha4.contains(elementClick)){
-        infoKombucha4.style.display = 'none';
-    }
-});
+if (btnKombucha4 != undefined){
+    btnKombucha4.addEventListener('click', function() {
+        infoKombucha4.style.display = 'flex';
+    });
+    document.addEventListener('click', function(event) {
+        let elementClick = event.target;
+        if (elementClick != btnKombucha4 && !infoKombucha4.contains(elementClick)){
+            infoKombucha4.style.display = 'none';
+        }
+    });
+}
 //----------------------------------
 //----------------------------------
-const btnPro = document.querySelector('.pro');
-const infoPro = document.querySelector('.detail-pro');
-btnPro.addEventListener('click', function() {
-  infoPro.style.display = 'flex';
-});
-document.addEventListener('click', function(event) {
-  let elementClick = event.target;
-  if (elementClick != btnPro && !infoPro.contains(elementClick)) {
-    infoPro.style.display = 'none';
-  }
-});
+const btnPro = document.getElementById('pro');
+const infoPro = document.getElementById('detail-pro');
+if (btnPro != undefined){
+    btnPro.addEventListener('click', function() {
+      infoPro.style.display = 'flex';
+    });
+    document.addEventListener('click', function(event) {
+      let elementClick = event.target;
+      if (elementClick != btnPro && !infoPro.contains(elementClick)) {
+        infoPro.style.display = 'none';
+      }
+    });
+}
 //----------------------------------
 const btnTraieur = document.getElementById('traiteur');
 const infoTraiteur = document.getElementById('detail-traiteur');
-btnTraieur.addEventListener('click', function() {
-    infoTraiteur.style.display = 'flex';
-});
-document.addEventListener('click', function(event) {
-    let elementClick = event.target;
-    if (elementClick != btnTraieur && !infoTraiteur.contains(elementClick)){
-        infoTraiteur.style.display = 'none';
-    }
-});
+if (btnTraieur != undefined){
+    btnTraieur.addEventListener('click', function() {
+        infoTraiteur.style.display = 'flex';
+    });
+    document.addEventListener('click', function(event) {
+        let elementClick = event.target;
+        if (elementClick != btnTraieur && !infoTraiteur.contains(elementClick)){
+            infoTraiteur.style.display = 'none';
+        }
+    });
+}
 //----------------------------------
 const btnAteliers = document.getElementById('ateliers');
 const infoAteliers = document.getElementById('detail-ateliers');
-btnAteliers.addEventListener('click', function() {
-    infoAteliers.style.display = 'flex';
-});
-document.addEventListener('click', function(event) {
-    let elementClick = event.target;
-    if (elementClick != btnAteliers && !infoAteliers.contains(elementClick)){
-        infoAteliers.style.display = 'none';
-    }
-});
+if (btnAteliers != undefined){
+    btnAteliers.addEventListener('click', function() {
+    
+        infoAteliers.style.display = 'flex';
+    });
+    document.addEventListener('click', function(event) {
+        let elementClick = event.target;
+        if (elementClick != btnAteliers && !infoAteliers.contains(elementClick)){
+            infoAteliers.style.display = 'none';
+        }
+    });
+}
